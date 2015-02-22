c9834d97e78a1c0cd9123d9d0027775269f4d6e8
Thomas Mortensson <thomasmortensson@gmail.com>
2015-02-22 20:55:17 +0000

    Added in verbose chat logging on server

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var io = require('socket.io')(8080);
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log('Chat message: ' +  msg);
   });
 });
