61b9d20ea86a6c69f63a87ed178e0fe9786298de
Thomas Mortensson <thomasmortensson@gmail.com>
2015-02-23 07:49:32 +0000

    Added in some commenting

--- www/chat.js
+++ www/chat.js
@@ -1,15 +1,26 @@
+
+// Open socket connection to server
 var socket = io('http://localhost:8080');
-      
+
+// When button clicked, send contents of textbox to server for broadcast to all clients      
 $('form').submit(function(){
 	socket.emit('chat message', $('#chatText').val());
 	$('#chatText').val('');
 	return false;
 });
 
+// Received message from server. Display on page
 socket.on('chat message', function(msg){
-	var user = $('<p>');
+	// We want to build a table row <tr> which contains: 
+	// - bold section <b>
+	// - paragraph text <p>
+	// As we wish them to be side by side we use the css property display: inline-block;
+
+	// Generate and populate bold block
 	var txt = $('<b>');
 	txt.text(msg.user + ': ');
+
+	var user = $('<p>');
 	user.append(txt);
 	user.css('display','inline-block');
 	
