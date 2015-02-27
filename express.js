13ad04b429e1d57026e0ab62c8a1458fb188b9cf
Thomas Mortensson <thomasmortensson@gmail.com>
2015-02-27 23:32:29 +0000

    Added simple express server

---
+++ express.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var server = express();
+server.use('/', express.static(__dirname + '/www'));
+
+var port = 3001;
+console.log("Serving on port: " + port);
+server.listen(port);
--- server.js
+++ server.js
@@ -15,3 +15,12 @@ io.on('connection', function(socket){
   });
 
 });
+
+// BELOW EXAMPLE EXPRESS SERVER
+// var express = require('express');
+// var server = express();
+// server.use('/', express.static(__dirname + '/www'));
+
+// var port = 3001;
+// console.log("Serving on port: " + port);
+// server.listen(port);
